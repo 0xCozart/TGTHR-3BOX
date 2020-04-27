bad92edcd7a171c818f3e7b7ab5a0c20929324d8
root <root@localhost.localdomain>
2020-04-27 04:32:08 +0000

    Code base ready

--- server/server.js
+++ server/server.js
@@ -4,3 +4,16 @@ const {
     getProfiles, getVerifiedAccounts
 } = require('3box/lib/api')
 
+// Using PROFILE API via 3box 
+// const profile = await Box.getProfile('0x12345abcde')
+// Get verified Twitter and Github accounts:
+// const verifiedAccounts = await Box.getVerifiedAccounts(profile)
+
+
+// Using GraphQL Endpoint
+
+const fields = ['name', 'website', 'favorite cereal']
+const values = ['Belphegor', 'tgthr.world', 'Honey Bunches']
+
+const response = async() => await Box.public.set('email', 'alanvegaop@gmail.com');
+console.log(response);
